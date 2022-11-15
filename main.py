83acd8d22a1063b4879efc1bc70e31c94bf8bbdc
malkolmz <kurban.63.axmedov@gmail.com>
2022-11-15 18:46:39 +0400

    add README

--- main.py
+++ main.py
@@ -25,12 +25,15 @@ for event in long_poll.listen():
                     sender(vk_user_id, text=bot_messages[message_from_user])
                 except KeyError:
                     try:
+                        # try:
                         if message_from_user[:5] == "поиск":
                             search, city, sex, age_from, age_to = message_from_user.split()
                             vk = BotFunction(vk_user_id=vk_user_id, city=city, sex=sex, age_from=age_from,
                                              age_to=age_to)
                             text_for_user, photos_for_user = vk.user_search_data()
                             sender(vk_user_id, text=text_for_user, photos=photos_for_user, keyboard=second_keyboard)
+                        # except KeyError:
+                        #     sender(vk_user_id, text=error_message["key_error"], keyboard=None)
 
                         if message_from_user == "следующий профиль":
                             text_for_user, photos_for_user = vk.next_profile()
